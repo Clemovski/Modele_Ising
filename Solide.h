bdb6d22c183879e79ae992821b4b29f17e23e255
SCHOTTE CLEMENT p0907637 <p0907637@b751pc161.univ-lyon1.fr>
2018-03-09 16:18:49 +0100

    Ajout des classes Solide et Solide1D

---
+++ Solide.h
@@ -0,0 +1,11 @@
+class Solide
+{
+private:
+	int largeur;
+	float couplageJ;
+	float champB;
+	float momentMag;
+
+public:
+	virtual ~Solide();
+};
---
+++ Solide1D.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include "Solide.h"
+
+class Solide1D : public Solide
+{
+private:
+	vector<char> elements;
+
+};
